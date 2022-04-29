201816acbc37ccd0464e2e7da47674b2a879262c
LightYearDeveloper <89242618+LightYearDeveloper@users.noreply.github.com>
2022-04-29 14:21:28 +0800

    anchor project

---
+++ programs/step-monsters-solana/src/lib.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+#[program]
+pub mod step_monsters_solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
