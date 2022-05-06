fa3f3480ed3bc68a4c61c35ae783ca2102f8e72d
LightYearDeveloper <89242618+LightYearDeveloper@users.noreply.github.com>
2022-05-06 16:07:29 +0800

    Update lib.rs

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -1,15 +1,104 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program::invoke;
-use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
-
 use anchor_spl::token;
 use anchor_spl::token::{MintTo, Token};
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod nft {
     use super::*;
+
+    pub fn mint_token(
+        ctx: Context<MintToken>,
+    ) -> Result<()> {
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::mint_to(cpi_ctx, 1)?;
+        Ok(())
+    }
+
+    pub fn create_account(
+        ctx: Context<CreateAccount>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.update_authority.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+        ];
+        let symbol = String::from("symbol");
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.update_authority.key(),
+                title,
+                symbol,
+                uri,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        Ok(())
+    }
+
+    pub fn create_master_edition(
+        ctx: Context<CreateMasterEdition>,
+    ) -> Result<()> {
+        let master_edition_infos = vec![
+            ctx.accounts.edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.update_authority.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.update_authority.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+        Ok(())
+    }
 
     pub fn mint_nft(
         ctx: Context<MintNFT>,
@@ -108,6 +197,40 @@ pub mod nft {
 }
 
 #[derive(Accounts)]
+pub struct MintToken<'info> {
+    pub token_program: Program<'info, Token>,
+    pub mint: AccountInfo<'info>,
+    pub token_account: AccountInfo<'info>,
+    pub payer: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct CreateAccount<'info> {
+    pub metadata: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>,
+    pub mint_authority: Signer<'info>,
+    pub payer: Signer<'info>,
+    pub update_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: AccountInfo<'info>,
+    pub token_metadata_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct CreateMasterEdition<'info> {
+    pub edition: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>,
+    pub update_authority: Signer<'info>,
+    pub mint_authority: Signer<'info>,
+    pub payer: AccountInfo<'info>,
+    pub metadata: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: AccountInfo<'info>,
+    pub token_metadata_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
 pub struct MintNFT<'info> {
     #[account(mut)]
     pub mint_authority: Signer<'info>,
@@ -125,5 +248,4 @@ pub struct MintNFT<'info> {
     pub rent: AccountInfo<'info>,
     #[account(mut)]
     pub master_edition: AccountInfo<'info>,
-
 }
