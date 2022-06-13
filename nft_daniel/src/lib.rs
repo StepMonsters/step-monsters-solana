33e793ff541c9f04fd77a80b879c3ec0b558ecbe
daniel-kung <dannielkung@gmail.com>
2022-06-13 21:21:21 +0800

    add program id

--- nft_daniel/src/lib.rs
+++ nft_daniel/src/lib.rs
@@ -7,4 +7,4 @@ pub mod utils;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("DcYeuAa91heGSNqHN9gVDUhBE4hRJo7BJT75zSRh1nuz");
+solana_program::declare_id!("F5XPMWxVTaxfCU1RUaLc4LNX1eFFx5u9For8EGLwPKPt");
