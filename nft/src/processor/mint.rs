0b241f428320c48842a2aee81be3c781a2622742
LightYearDeveloper <89242618+LightYearDeveloper@users.noreply.github.com>
2022-07-06 16:28:20 +0800

    create metadata edition

--- nft/src/processor/mint.rs
+++ nft/src/processor/mint.rs
@@ -1,10 +1,9 @@
 use borsh::BorshSerialize;
-use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 use solana_program::{
     account_info::{AccountInfo, next_account_info},
     entrypoint::ProgramResult,
     msg,
-    program::{invoke, invoke_signed},
+    program::invoke,
     program_error::ProgramError,
     pubkey::Pubkey,
     system_instruction,
@@ -12,7 +11,7 @@ use solana_program::{
 };
 
 use crate::{ferror, state::*, utils::*};
-use crate::utils_mint::create_monster_info;
+use crate::utils_mint::{create_metadata_edition, create_monster_info};
 
 pub fn process_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
@@ -37,15 +36,6 @@ pub fn process_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramRes
     assert_signer(&signer_info)?;
     assert_config(&program_id, &config_info)?;
 
-    let pda_bump = assert_pda_creator(&program_id, pda_creator_info)?;
-
-    let pda_seed = [
-        SEED_BATTLE.as_bytes(),
-        program_id.as_ref(),
-        "pda_creator".as_bytes(),
-        &[pda_bump],
-    ];
-
     let mut config_data = ConfigureData::from_account_info(config_info)?;
     assert_eq_pubkey(&fee_receiver_info, &config_data.fee_receiver)?;
 
@@ -65,77 +55,22 @@ pub fn process_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramRes
         )?;
     }
 
-    let creators = vec![
-        mpl_token_metadata::state::Creator {
-            address: *pda_creator_info.key,
-            verified: true,
-            share: 0,
-        },
-        mpl_token_metadata::state::Creator {
-            address: config_data.creator,
-            verified: false,
-            share: 100,
-        },
-    ];
-
-    let name_id = String::from(" #") + &config_data.current_id.to_string();
-    msg!("Create metadata");
-    invoke_signed(
-        &create_metadata_accounts_v2(
-            *metadata_program_info.key,
-            *metadata_info.key,
-            *mint_info.key,
-            *signer_info.key,
-            *signer_info.key,
-            *pda_creator_info.key, //pda must be signer
-            config_data.name.clone() + &name_id,
-            config_data.symbol.clone(),
-            config_data.uri.clone(),
-            Some(creators),
-            config_data.fee,
-            true,
-            true,
-            None,
-            None,
-        ),
-        &[
-            metadata_info.clone(),
-            mint_info.clone(),
-            signer_info.clone(),
-            metadata_program_info.clone(),
-            token_program_info.clone(),
-            system_info.clone(),
-            rent_info.clone(),
-            pda_creator_info.clone(),
-        ],
-        &[&pda_seed],
+    msg!("Create Metadata Edition");
+    create_metadata_edition(
+        &program_id,
+        &pda_creator_info,
+        config_data.clone(),
+        &signer_info,
+        &mint_info,
+        &metadata_info,
+        &edition_info,
+        &metadata_program_info,
+        &token_program_info,
+        &system_info,
+        &rent_info,
     )?;
-
-    msg!("Create Master Edition");
-    invoke_signed(
-        &create_master_edition_v3(
-            *metadata_program_info.key,
-            *edition_info.key,
-            *mint_info.key,
-            *pda_creator_info.key,
-            *signer_info.key,
-            *metadata_info.key,
-            *signer_info.key,
-            Some(1),
-        ),
-        &[
-            edition_info.clone(),
-            mint_info.clone(),
-            signer_info.clone(),
-            metadata_info.clone(),
-            metadata_program_info.clone(),
-            token_program_info.clone(),
-            system_info.clone(),
-            rent_info.clone(),
-            pda_creator_info.clone(),
-        ],
-        &[&pda_seed],
-    )?;
+    config_data.current_id += 1;
+    config_data.serialize(&mut *config_info.try_borrow_mut_data()?)?;
 
     msg!("Create Monster Info");
     create_monster_info(
@@ -152,7 +87,5 @@ pub fn process_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramRes
     monster.monster_feature = Vec::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     monster.serialize(&mut *monster_info.try_borrow_mut_data()?)?;
 
-    config_data.current_id += 1;
-    config_data.serialize(&mut *config_info.try_borrow_mut_data()?)?;
     Ok(())
 }
--- nft/src/utils_mint.rs
+++ nft/src/utils_mint.rs
@@ -1,9 +1,12 @@
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 use solana_program::account_info::AccountInfo;
+use solana_program::msg;
+use solana_program::program::invoke_signed;
 use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
 
-use crate::state::{MAX_MONSTER_LENGTH, SEED_MONSTER};
-use crate::utils::{assert_derivation, create_or_allocate_account_raw};
+use crate::state::{ConfigureData, MAX_MONSTER_LENGTH, SEED_BATTLE, SEED_MONSTER};
+use crate::utils::{assert_derivation, assert_pda_creator, create_or_allocate_account_raw};
 
 pub fn create_monster_info<'a>(
     program_id: &Pubkey,
@@ -36,6 +39,103 @@ pub fn create_monster_info<'a>(
         signer_info,
         MAX_MONSTER_LENGTH,
         monster_seeds,
+    )?;
+
+    Ok(())
+}
+
+pub fn create_metadata_edition<'a>(
+    program_id: &Pubkey,
+    pda_creator_info: &AccountInfo<'a>,
+    config_data: ConfigureData,
+    signer_info: &AccountInfo<'a>,
+    mint_info: &AccountInfo<'a>,
+    metadata_info: &AccountInfo<'a>,
+    edition_info: &AccountInfo<'a>,
+    metadata_program_info: &AccountInfo<'a>,
+    token_program_info: &AccountInfo<'a>,
+    system_info: &AccountInfo<'a>,
+    rent_info: &AccountInfo<'a>
+) -> Result<(), ProgramError> {
+
+    let pda_bump = assert_pda_creator(&program_id, pda_creator_info)?;
+    let pda_seed = [
+        SEED_BATTLE.as_bytes(),
+        program_id.as_ref(),
+        "pda_creator".as_bytes(),
+        &[pda_bump],
+    ];
+
+    let creators = vec![
+        mpl_token_metadata::state::Creator {
+            address: *pda_creator_info.key,
+            verified: true,
+            share: 0,
+        },
+        mpl_token_metadata::state::Creator {
+            address: config_data.creator,
+            verified: false,
+            share: 100,
+        },
+    ];
+
+    let name_id = String::from(" #") + &config_data.current_id.to_string();
+    msg!("Create Metadata");
+    invoke_signed(
+        &create_metadata_accounts_v2(
+            *metadata_program_info.key,
+            *metadata_info.key,
+            *mint_info.key,
+            *signer_info.key,
+            *signer_info.key,
+            *pda_creator_info.key, //pda must be signer
+            config_data.name.clone() + &name_id,
+            config_data.symbol.clone(),
+            config_data.uri.clone(),
+            Some(creators),
+            config_data.fee,
+            true,
+            true,
+            None,
+            None,
+        ),
+        &[
+            metadata_info.clone(),
+            mint_info.clone(),
+            signer_info.clone(),
+            metadata_program_info.clone(),
+            token_program_info.clone(),
+            system_info.clone(),
+            rent_info.clone(),
+            pda_creator_info.clone(),
+        ],
+        &[&pda_seed],
+    )?;
+
+    msg!("Create Master Edition");
+    invoke_signed(
+        &create_master_edition_v3(
+            *metadata_program_info.key,
+            *edition_info.key,
+            *mint_info.key,
+            *pda_creator_info.key,
+            *signer_info.key,
+            *metadata_info.key,
+            *signer_info.key,
+            Some(1),
+        ),
+        &[
+            edition_info.clone(),
+            mint_info.clone(),
+            signer_info.clone(),
+            metadata_info.clone(),
+            metadata_program_info.clone(),
+            token_program_info.clone(),
+            system_info.clone(),
+            rent_info.clone(),
+            pda_creator_info.clone(),
+        ],
+        &[&pda_seed],
     )?;
 
     Ok(())
