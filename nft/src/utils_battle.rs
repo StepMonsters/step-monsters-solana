06fc8e1976278eb8bb02471af6b14b149d7b95ab
LightYearDeveloper <89242618+LightYearDeveloper@users.noreply.github.com>
2022-12-12 14:27:46 +0800

    update battle round

--- nft/src/utils_battle.rs
+++ nft/src/utils_battle.rs
@@ -5,20 +5,34 @@ pub fn battle_round(monster: Monster, args: BattleArgs) -> (u8, Vec<u32>) {
     let mut enemy_hp = args.hp.clone();
     let me_attack = monster.attack.clone();
     let enemy_attack = args.attack.clone();
+    let me_defense = monster.defense.clone();
+    let enemy_defense = args.defense.clone();
+    let me_speed = monster.speed.clone();
+    let enemy_speed = args.speed.clone();
+    let mut me_distance = 1000;
+    let mut enemy_distance = 1000;
 
     let mut arr = Vec::new();
 
     for _i in 0..20 {
-        enemy_hp = safe_sub(enemy_hp, me_attack / 10);
-        arr.push(enemy_hp * 10 + 1);
-        if enemy_hp <= 0 {
-            break;
-        }
-
-        me_hp = safe_sub(me_hp, enemy_attack / 10);
-        arr.push(me_hp * 10 + 2);
-        if me_hp <= 0 {
-            break;
+        if me_distance / me_speed <= enemy_distance / enemy_speed {
+            enemy_distance -= (me_distance / me_speed) * enemy_speed;
+            me_distance = 1000;
+            let damage = apply_attack(me_attack, enemy_defense);
+            enemy_hp = safe_sub(enemy_hp, damage);
+            arr.push(enemy_hp * 10 + 1);
+            if enemy_hp <= 0 {
+                break;
+            }
+        } else {
+            me_distance -= (enemy_distance / enemy_speed) * me_speed;
+            enemy_distance = 1000;
+            let damage = apply_attack(enemy_attack, me_defense);
+            me_hp = safe_sub(me_hp, damage);
+            arr.push(me_hp * 10 + 2);
+            if me_hp <= 0 {
+                break;
+            }
         }
     }
 
@@ -32,46 +46,6 @@ pub fn battle_round(monster: Monster, args: BattleArgs) -> (u8, Vec<u32>) {
         win = 1;
     }
     return (win, arr);
-
-    // if monster.hp >= args.hp {
-    //     return 1;
-    // };
-    // return 0;
-
-    // let mut me_hp = monster.hp.clone();
-    // let mut enemy_hp = args.hp.clone();
-    // let mut me_time = 10000;
-    // let mut enemy_time = 10000;
-    // let me_speed = monster.speed;
-    // let enemy_speed = args.speed;
-    //
-    // let win;
-    //
-    // loop {
-    //     let is_my_turn;
-    //     if me_time / me_speed < enemy_time / enemy_speed {
-    //         enemy_time = safe_sub(enemy_time, me_time / enemy_speed);
-    //         is_my_turn = true;
-    //     } else {
-    //         me_time = safe_sub(me_time, enemy_time / me_speed);
-    //         is_my_turn = false;
-    //     }
-    //
-    //     if is_my_turn {
-    //         let damage = apply_attack(monster.attack, args.defense);
-    //         enemy_hp = safe_sub(enemy_hp, damage);
-    //     } else {
-    //         let damage = apply_attack(args.attack, monster.defense);
-    //         me_hp = safe_sub(me_hp, damage);
-    //     }
-    //
-    //     if me_hp <= 0 || enemy_hp <= 0 {
-    //         win = me_hp > 0;
-    //         break;
-    //     }
-    // }
-    //
-    // return win;
 }
 
 pub fn apply_attack(attack: u32, defense: u32) -> u32 {
@@ -87,7 +61,7 @@ pub fn apply_attack(attack: u32, defense: u32) -> u32 {
         damage = attack - defense * 7 / 100;
     }
 
-    return damage;
+    return damage / 3;
 }
 
 pub fn safe_sub(a: u32, b: u32) -> u32 {
